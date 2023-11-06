9e942914e16cff381d1683c85d8af791c5ce6525
Adam Metz <alm939@uregina.ca>
2023-11-05 20:53:51 -0600

    Handle ensuring form is filled before submission

--- src/components/Form/FileTag.tsx
+++ src/components/Form/FileTag.tsx
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function FileTag() {
+interface FileTagProps {
+  updateTags: (newTags: string[]) => void;
+}
+
+const FileTag: React.FC<FileTagProps> = ({ updateTags }) => {
   const inputStyles =
     "block w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6";
-  const [chips, setChips] = useState<string[]>([]);
-  const [currentChip, setCurrentChip] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
+  const [currentTag, setCurrentTag] = useState<string>("");
 
-  const handleInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
+  useEffect(() => {
+    updateTags(tags)
+  }, [tags]);
+
+  const handleTagEntry = (e: React.KeyboardEvent<HTMLInputElement> | React.FocusEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" || e.type === "blur") {
       e.preventDefault(); // Prevent form submission
-      if (currentChip.trim() !== "") {
-        setChips([...chips, currentChip]);
-        setCurrentChip("");
+      if (currentTag.trim() !== "") {
+        setTags([...tags, currentTag]);
+        setCurrentTag("");
       }
     }
   };
 
-  const removeChip = (index: number) => {
-    const updatedChips = chips.filter((_, i) => i !== index);
-    setChips(updatedChips);
+  const removeTag = (index: number) => {
+    const updatedTags = tags.filter((_, i) => i !== index);
+    setTags(updatedTags);
   };
 
   return (
@@ -28,22 +36,23 @@ function FileTag() {
         className={inputStyles}
         type="text"
         id="fileTags"
-        value={currentChip}
-        onChange={(e) => setCurrentChip(e.target.value)}
-        onKeyDown={handleInputKeyPress}
+        value={currentTag}
+        onChange={(e) => setCurrentTag(e.target.value)}
+        onBlur={handleTagEntry}
+        onKeyDown={handleTagEntry}
       />
       <div className="flex flex-wrap gap-2">
-        {chips.map((chip, index) => (
+        {tags.map((tag, index) => (
           <div
             key={index}
             className="m-1 flex items-center justify-center rounded-full border border-green-300 bg-green-100 px-2 py-1 font-medium text-green-700"
           >
             <div className="max-w-full flex-initial text-xs font-normal leading-none">
-              {chip}
+              {tag}
             </div>
             <div
               className="flex flex-auto flex-row-reverse"
-              onClick={() => removeChip(index)}
+              onClick={() => removeTag(index)}
             >
               <div>
                 <svg
@@ -53,9 +62,9 @@ function FileTag() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="feather feather-x ml-2 h-4 w-4 cursor-pointer rounded-full hover:text-indigo-400"
                 >
                   <line x1="18" y1="6" x2="6" y2="18"></line>
@@ -68,6 +77,6 @@ function FileTag() {
       </div>
     </>
   );
-}
+};
 
 export default FileTag;

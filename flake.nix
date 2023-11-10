ac6195da19cf5e2474edfe548191ead4974d6bad
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-11-10 08:32:31 -0600

    add action, cleanup flake to test action

--- flake.nix
+++ flake.nix
@@ -3,18 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    # rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
+    # rust-overlay,
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [rust-overlay.overlays.default];
+      # overlays = [rust-overlay.overlays.default];
     };
 
     # toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;

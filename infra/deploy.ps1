81726dcd779ccc0477d4d3dc7960b38e5d31f72d
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-02-01 23:07:28 -0600

    Revert "Update deploy scripts to conditionally use mock data"

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -1,6 +1,5 @@
 param (
-    [string]$LocalStackApiKey=""
-    [bool]$useMockData = $false # Data is pulled from /infra/useCases/mockUseCasesBatchCommand.js
+    [string]$LocalStackApiKey
 )
 
 if ($null -eq $LocalStackApiKey -or $LocalStackApiKey -eq "") {
@@ -102,14 +101,12 @@ docker rmi "$repoUri";
 
 Write-Output "Creating ECS infrastructure locally";
 awslocal cloudformation create-stack --stack-name infra --template-body file://infra/private.vpc.yml;
-Start-Sleep -Seconds 2
 Start-SleepTillStacksCreated -stackName "infra"
 
 Write-Output "Deploying ECS app to local environment"
 awslocal cloudformation create-stack --stack-name nardo `
     --template-body file://infra/private.nardo.yml `
     --parameters ParameterKey=ImageUrl,ParameterValue=$repoUri;
-Start-Sleep -Seconds 2
 Start-SleepTillStacksCreated -stackName "nardo"
 
 Write-Output "ECS app successfully deployed. Trying to access app endpoint."
@@ -140,6 +137,4 @@ Write-Output "Tables: $tables\n"
 $queues = awslocal sqs list-queues
 Write-Output "Queues: $queues\n"
 
-if ($useMockData) {
-    awslocal dynamodb batch-write-item --request-items file://./infra/useCases/mockUseCasesBatchCommand.json
-}
+awslocal dynamodb batch-write-item --request-items file://./infra/useCases/mockUseCasesBatchCommand.json

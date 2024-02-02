d27078ebf8506b8918deb33dc24bb0a007d18935
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-02-01 20:37:29 -0600

    Remove analysis type router delay

--- src/server/api/routers/analysis.ts
+++ src/server/api/routers/analysis.ts
@@ -2,11 +2,6 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const analysisRouter = createTRPCRouter({
   getAnalysisTypes: publicProcedure.query(async () => {
-    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    // Introduce a delay for testing loading elements
-    await delay(100);
-
     return {
       types: [
         { name: "Rolling Mean", id: 1 },

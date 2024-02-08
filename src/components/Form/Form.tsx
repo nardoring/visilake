a64f5754cd90af8630cae44a120453ff2a294c32
Adam Metz <adam.l.metz@gmail.com>
2024-02-07 19:58:45 -0600

    Add spinner component to submit use case button when loading

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -21,7 +21,6 @@ export default function Form() {
   const [showPopup, setShowPopup] = useState(false);
   const [formSuccess, setFormSuccess] = useState(false);
 
-
   const analysisTypeOptions: AnalysisTypeOption[] = analysisTypeOptionsIsLoading
     ? []
     : analysisTypeOptionsData?.types?.map(
@@ -36,7 +35,9 @@ export default function Form() {
   };
 
   const getValidTags = (): string[] => {
-    return tags.filter((tag) => tag.isValid === true).map((tag) => tag.name) || [];
+    return (
+      tags.filter((tag) => tag.isValid === true).map((tag) => tag.name) || []
+    );
   };
 
   // Prevent Enter key from submitting the form
@@ -58,7 +59,9 @@ export default function Form() {
       getValidTags().length !== 0 &&
       useCaseDescription.trim() !== ""
     ) {
-      const analysisTypeNames: string[] = analysisTypes.map((type) => type.label);
+      const analysisTypeNames: string[] = analysisTypes.map(
+        (type) => type.label,
+      );
       try {
         await useCaseSubmission.mutateAsync({
           tags: getValidTags(),
@@ -76,7 +79,11 @@ export default function Form() {
   }
 
   return (
-    <form className="mx-auto max-w-screen-md p-4 " onSubmit={handleSubmit} id="useSubmissionCaseForm">
+    <form
+      className="mx-auto max-w-screen-md p-4 "
+      onSubmit={handleSubmit}
+      id="useSubmissionCaseForm"
+    >
       <div className="font-nunito mt-10 grid grid-cols-2 gap-x-6 gap-y-4 rounded border border-slate-400 bg-lightBlue p-4 font-medium shadow-md">
         <div>
           <label htmlFor="useCaseTitle">Use Case Title</label>
@@ -134,15 +141,41 @@ export default function Form() {
             onChange={(e) => setUseCaseDescription(e.target.value)}
           />
         </div>
-        <div className="col-span-2 flex justify-center">
+        <div className="col-span-2 flex justify-center ">
           <button
-            className="rounded bg-veryDarkBlue px-4 py-2 text-white shadow-md hover:bg-darkBlue"
+            className="w-40 rounded bg-veryDarkBlue px-4 py-2 text-white shadow-md hover:bg-darkBlue flex justify-center items-center"
             type="submit"
+            disabled={useCaseSubmission.isLoading}
           >
-            Submit Use Case
+            {useCaseSubmission.isLoading ? (
+              <div>
+                <svg
+                  className="text-white-200 h-6 w-6 animate-spin fill-white dark:text-gray-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : (
+              "Submit Use Case"
+            )}
           </button>
         </div>
-        <FormPopup formSuccess={formSuccess} showPopup={showPopup} setShowPopup={setShowPopup}/>
+        <FormPopup
+          formSuccess={formSuccess}
+          showPopup={showPopup}
+          setShowPopup={setShowPopup}
+        />
       </div>
     </form>
   );

4c6f1d07152ad134087ea4d99d30ee0d57a91abf
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2024-02-10 06:11:55 -0600

    WIP-start intersecting background

---
+++ src/components/Background.tsx
@@ -0,0 +1,11 @@
+const Background = () => {
+  return (
+    <div className="background-grid">
+      <div className="parallelogram-three area-three z-0"></div>
+      <div className="parallelogram-one area-one z-10"></div>
+      <div className="parallelogram-two area-two z-10"></div>
+    </div>
+  );
+};
+
+export default Background;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import Background from "../components/Background";
 import Navbar from "../components/Navbar";
 import Form from "../components/Form/Form";
 
 export default function FormPage() {
   return (
     <>
-      <main className="min-h-screen bg-lightGrey">
+      <main className="min-h-screen ">
+        <Background />
         <Navbar />
         <Form />
       </main>

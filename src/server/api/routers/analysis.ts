8a93b3d76137f2ec54cc19d9866c6f0ed045436f
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2024-02-12 04:31:24 +0000

    Refactored client initialization to use env vars

--- src/server/api/routers/analysis.ts
+++ src/server/api/routers/analysis.ts
@@ -1,18 +1,14 @@
-import AWS from "aws-sdk";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import mapAnalysisTypes from "~/mappers/analysisTypeMapper";
 import type { AnalysisType } from "~/models/db/analysisType";
 import type { DynamoScan } from "~/models/db/dynamoScan";
+import getDynamoDBClient from "~/clients/dynamodb";
 
 const DYNAMODB_TABLE = "analysisTypes";
 
 export const analysisRouter = createTRPCRouter({
   getAnalysisTypes: publicProcedure.query(async () => {
-    // TODO fix endpoint
-    const dynamodb = new AWS.DynamoDB({
-      endpoint: "http://dynamodb.us-east-1.localhost.localstack.cloud:4566/",
-      region: "us-east-1",
-    });
+    const dynamodb = getDynamoDBClient();
 
     const result = (await dynamodb
       .scan({

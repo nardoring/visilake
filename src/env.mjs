5aeb0e8a4624e28426c837862d7b76ad1040e025
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2024-02-19 20:53:35 +0000

    Added back node env to environment variables

--- src/env.mjs
+++ src/env.mjs
@@ -6,7 +6,11 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {},
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -21,7 +25,9 @@ export const env = createEnv({
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
    * middlewares) or client-side so we need to destruct manually.
    */
-  runtimeEnv: {},
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
    * This is especially useful for Docker builds.

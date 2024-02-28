cea25ed88475b7989a2f056972662dce0ca54fdc
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-02-28 14:09:25 -0600

    Add PowerBI Link Column and Grid Options param

--- src/components/ListPage/UseCaseTable.tsx
+++ src/components/ListPage/UseCaseTable.tsx
@@ -4,6 +4,7 @@ import { AgGridReact } from "ag-grid-react";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import PowerBIButton from "./PowerBIButton";
 
 export default function UseCaseTable() {
   const [queryExecuted, setQueryExecuted] = useState<boolean>(false);
@@ -16,13 +17,21 @@ export default function UseCaseTable() {
   });
 
   const [colDefs, setColDefs] = useState([
-    { field: "useCaseName", headerName: "Job Title"},
-    { field: "useCaseDescription", headerName: "Job Description"},
+    { field: "useCaseName", headerName: "Job Title" },
+    { field: "useCaseDescription", headerName: "Job Description" },
     { field: "analysisTypes" },
     { field: "date" },
     { field: "author" },
     { field: "useCaseStatus" },
+    {
+      field: "powerBILink",
+      cellRenderer: PowerBIButton,
+    },
   ]);
+
+  const gridOptions = {
+    rowHeight: 75,
+  }
 
   if (isLoading) {
     // Render a loading indicator or message
@@ -34,16 +43,14 @@ export default function UseCaseTable() {
   }
 
   return (
-    <div 
-    className="col-start-2 col-end-9 row-start-2 mb-5 mt-5"
-    >
+    <div className="col-start-2 col-end-9 row-start-2 mb-5 mt-5">
       <div
         className="relative z-20 col-start-2 col-end-9 row-start-3 row-end-4
                      flex h-[64rem] flex-col
                     overflow-x-auto rounded-md"
       >
         <div className={"ag-theme-quartz"} style={{ height: "500px" }}>
-          <AgGridReact rowData={data} columnDefs={colDefs} />
+          <AgGridReact rowData={data} columnDefs={colDefs} gridOptions={gridOptions}/>
         </div>
       </div>
     </div>

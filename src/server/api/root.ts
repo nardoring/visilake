3b17a621a6e8461ee95ab1a7f080c310260383e6
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2024-03-07 03:46:58 +0000

    Readding the endpoints for the queue getting

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,6 +3,7 @@ import { createTRPCRouter } from '~/server/api/trpc';
 import { analysisRouter } from './routers/analysis';
 import { jobRouter } from './routers/job';
 import { sourceRouter } from './routers/source';
+import { jobUpdatesRouter } from './routers/jobUpdates';
 
 /**
  * This is the primary router for your server.
@@ -14,6 +15,7 @@ export const appRouter = createTRPCRouter({
   analysis: analysisRouter,
   job: jobRouter,
   source: sourceRouter,
+  jobUpdates: jobUpdatesRouter,
 });
 
 // export type definition of API

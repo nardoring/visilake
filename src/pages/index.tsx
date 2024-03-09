f74a0b6fb81c52b8b36b9e4af9be133cc6a842f5
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-09 11:08:21 -0600

    Add toast notifications for tag validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout';
 import Form from '../components/Form/Form';
+import { ToastContainer } from 'react-toastify';
 
 export default function FormPage() {
   return (
@@ -7,6 +8,18 @@ export default function FormPage() {
       <main className='min-h-screen overflow-x-hidden'>
         <Layout>
           <Form />
+          <ToastContainer
+            position='bottom-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='light'
+          />
         </Layout>
       </main>
     </>

29fb920edfd88a6d1bb2114f793069cc921d58e9
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2024-03-09 22:05:31 -0600

    serde of vectord status and types in response

--- nardo-proc/models/mod.rs
+++ nardo-proc/models/mod.rs
@@ -1,5 +1,6 @@
 pub mod data;
 pub mod job_queue;
 pub mod job_request;
+pub mod job_response;
 pub mod job_type;
 pub mod status;
--- nardo-proc/models/status.rs
+++ nardo-proc/models/status.rs
@@ -71,6 +71,14 @@ where
     serializer.serialize_str(&status.to_string())
 }
 
+pub fn serialize_statuses<S>(statuses: &[Status], serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let statuses_str: Vec<String> = statuses.iter().map(|jt| jt.to_string()).collect();
+    serializer.serialize_some(&statuses_str)
+}
+
 pub fn deserialize_status<'de, D>(deserializer: D) -> Result<Status, D::Error>
 where
     D: Deserializer<'de>,
@@ -93,4 +101,38 @@ where
     }
 
     deserializer.deserialize_str(StatusVisitor)
+}
+
+pub fn deserialize_statuses<'de, D>(deserializer: D) -> Result<Vec<Status>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct StatusesVisitor;
+
+    impl<'de> Visitor<'de> for StatusesVisitor {
+        type Value = Vec<Status>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a list of strings representing job statuses")
+        }
+
+        fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+        where
+            A: de::SeqAccess<'de>,
+        {
+            let mut statuses = Vec::new();
+
+            while let Some(job_status_str) = seq.next_element::<String>()? {
+                statuses.push(
+                    job_status_str
+                        .parse::<Status>()
+                        .map_err(de::Error::custom)?,
+                );
+            }
+
+            Ok(statuses)
+        }
+    }
+
+    deserializer.deserialize_seq(StatusesVisitor)
 }

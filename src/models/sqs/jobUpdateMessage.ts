4539d0fcb3dce276e5c64ab08f211bd8a163a913
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2024-03-12 03:59:23 +0000

    updated the job update model to include the rest of the fields

--- src/models/sqs/jobUpdateMessage.ts
+++ src/models/sqs/jobUpdateMessage.ts
@@ -3,4 +3,9 @@ import { JobStatus } from '../domain/jobStatus';
 export type JobUpdateMessage = {
   status: JobStatus;
   request_id: string;
+  analysis_types: string[];
+  timestamp: number;
+  author: string;
+  name: string;
+  description: string;
 };

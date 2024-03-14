12b00c32403daf1a0f0aa8836574b91467326e11
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-14 10:38:24 -0600

    Add basic layout for HelpAccordion

--- src/components/HelpPage/HelpAccordion.tsx
+++ src/components/HelpPage/HelpAccordion.tsx
@@ -1,6 +1,47 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 export default function HelpAccordion() {
   return (
-    <div>
+    <div className='pt-6'>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          id='panel1-analysisTypes'
+        >
+          Analysis Types
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          id='panel2-sources'
+        >
+          Sources
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          id='panel3-granularity'
+        >
+          Granularity
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }

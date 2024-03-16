5acb2506d6e9404f4400ef49d81b24ffa4627f07
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-15 20:43:30 -0600

    Cleanup SourcesSection

--- src/components/HelpPage/AccordionSections/SourcesSection.tsx
+++ src/components/HelpPage/AccordionSections/SourcesSection.tsx
@@ -1,75 +1,96 @@
 import AccordionDetails from '@mui/material/AccordionDetails';
-import { SetStateAction, useState } from 'react';
 import {
-  Accordion,
-  Icon,
   List,
   ListItem,
-  TextField,
   Typography,
 } from '@mui/material';
-import Sources from '~/components/Form/Sources';
-import { Source } from '~/utils/types';
-import CheckIcon from '@mui/icons-material/Check';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Image from 'next/image';
 
-const sourceTagValidator = RegExp(/^\d{4}-[A-Z]{2,3}-\d{5}$/);
+const examples = {
+  'Flow Instruments': [
+    {
+      tag: '1001-FI-50230',
+      description: 'Flow Indicator in area 1001, device number 50230',
+    },
+    {
+      tag: '1002-FT-50440',
+      description: 'Flow Transmitter in area 1002, device number 50440',
+    },
+    {
+      tag: '1003-FIC-50650',
+      description:
+        'Flow Indicating Controller in area 1003, device number 50650',
+    },
+  ],
+  'Temperature Instruments': [
+    {
+      tag: '2001-TI-60210',
+      description: 'Temperature Indicator in area 2001, device number 60210',
+    },
+    {
+      tag: '2002-TT-60420',
+      description: 'Temperature Transmitter in area 2002, device number 60420',
+    },
+    {
+      tag: '2003-TIC-60630',
+      description:
+        'Temperature Indicating Controller in area 2003, device number 60630',
+    },
+  ],
+  'Pressure Instruments': [
+    {
+      tag: '3001-PI-70270',
+      description: 'Pressure Indicator in area 3001, device number 70270',
+    },
+    {
+      tag: '3002-PT-70480',
+      description: 'Pressure Transmitter in area 3002, device number 70480',
+    },
+    {
+      tag: '3003-PIC-70690',
+      description:
+        'Pressure Indicating Controller in area 3003, device number 70690',
+    },
+  ],
+};
 
 export default function SourcesSection() {
-  const [source, setSource] = useState<string>('');
-  const [validSource, setValidSource] = useState<boolean>(false);
-
   return (
-    <div>
-      <AccordionDetails>
-        <Typography className='pl-2'>
+    <AccordionDetails>
+      <div className='pl-5'>
+        <Typography className='pl-2 pb-3'>
           The Sources input enables you to enter the source tags for the data
-          you would like to analyze. <br /> The format used for the tags, is a custom
-          ISA 5.1 format: 
-          <strong>1234-AB(C)-12345</strong>
-          <br />
+          you would like to analyze. The format used for the tags is a custom
+          ISA 5.1 format:
         </Typography>
-        <List className='pl-5'>
-          <ListItem>
-            <strong>'1234' - Area Location Code - Must be 4 digits</strong> <br />
-          </ListItem>
-          <ListItem>
-            <strong>'AB' or 'ABC'  - ISA Tag Standard Identifier - Must be 2-3 letters</strong>
-          </ListItem>
-          <ListItem>
-            <strong>'12345' - Device Number - Must be 5 digits</strong> 
-          </ListItem>
-        </List>
-        <Typography variant='h5'>Examples</Typography>
-        <br />
-
-        <br />
-        <Typography variant='h5'>Try it Yourself</Typography>
-        <br />
-        <div className='flex px-3'>
-          <TextField
-            placeholder='1234-AB(C)-12345'
-            value={source}
-            onChange={(e) => {
-              setSource(e.target.value);
-              setValidSource(sourceTagValidator.test(e.target.value));
-            }}
-          />
-          <Typography className='px-5 py-4'>
-            <span
-              className={`${validSource ? 'text-green-500' : 'text-red-500'}`}
-            >
-              {source}
-            </span>{' '}
-            is {validSource ? 'valid' : 'invalid'}.
-          </Typography>
-          <CheckIcon
-            style={{ fill: 'green' }}
-            fontSize='large'
-          />
+        <div className='flex items-center justify-center'>
+          <div className='mx-auto my-auto h-1/2 w-1/2'>
+            <Image
+              src={'/Source-Tag-Format.png'}
+              alt={''}
+              width={1892}
+              height={567}
+            />
+          </div>
         </div>
-      </AccordionDetails>
-    </div>
+        <Typography variant='h5'>
+          <strong>Examples</strong>
+        </Typography>
+        <div className='py-5 pl-5'>
+          {Object.entries(examples).map(([category, tags]) => (
+            <div key={category}>
+              <Typography variant='h6'>{category}</Typography>
+              <List>
+                {tags.map(({ tag, description }) => (
+                  <ListItem key={tag}>
+                    <strong>{tag}:</strong>&nbsp;{description}
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          ))}
+        </div>
+      </div>
+    </AccordionDetails>
   );
 }

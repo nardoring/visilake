1b79eb9d9884f76b1c9f67d6467f656c1789816d
AdamMetz <alm939@uregina.ca>
2024-03-20 19:51:05 -0600

    Add background to Additional Information section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,9 @@ export default function FormPage() {
       <main className='min-h-screen overflow-x-hidden'>
         <Layout>
           <Form />
-          <div className='px-4 pt-6'>
+          <div className='px-12 pt-6 pb-12 rounded border border-slate-400 bg-veryLightGrey p-4 font-medium shadow-md mt-10 mx-4'>
             <Typography
-              className='text-bold text-center text-white'
+              className='text-bold text-center text-black'
               variant='h4'
               sx={{ fontWeight: 'bold' }}
             >

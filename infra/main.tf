3e59c5c9f0b3b1b240aa53ab41be5a92a6d8e94d
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2024-03-23 04:07:05 +0000

    Removed extra queues in terraform

--- infra/main.tf
+++ infra/main.tf
@@ -152,70 +152,10 @@ resource "aws_sqs_queue" "request_queue" {
   message_retention_seconds = 60 # for localstack only
 }
 
-resource "aws_sqs_queue" "request_updates_1" {
-  name                      = "requestUpdatesQueue-1"
-  message_retention_seconds = 60 # for localstack only
-
-  # TODO tag everything properly
-  # tags = {
-  #   Environment = "development"
-  # }
-}
-
-resource "aws_sqs_queue" "request_updates_2" {
-  name                      = "requestUpdatesQueue-2"
-  message_retention_seconds = 60 # for localstack only
-}
-
-resource "aws_sqs_queue" "request_updates_3" {
-  name                      = "requestUpdatesQueue-3"
-  message_retention_seconds = 60 # for localstack only
-}
-
-resource "aws_sqs_queue" "request_updates_4" {
-  name                      = "requestUpdatesQueue-4"
-  message_retention_seconds = 60 # for localstack only
-}
-
-resource "aws_sqs_queue" "request_updates_5" {
-  name                      = "requestUpdatesQueue-5"
-  message_retention_seconds = 60 # for localstack only
-}
-
 resource "aws_sns_topic" "request_updates" {
   name                        = "requestUpdatesTopic.fifo"
   fifo_topic                  = true
   content_based_deduplication = true
-}
-
-resource "aws_sns_topic_subscription" "request_updates_sqs_target1" {
-  topic_arn = aws_sns_topic.request_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.request_updates_1.arn
-}
-
-resource "aws_sns_topic_subscription" "request_updates_sqs_target2" {
-  topic_arn = aws_sns_topic.request_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.request_updates_2.arn
-}
-
-resource "aws_sns_topic_subscription" "request_updates_sqs_target3" {
-  topic_arn = aws_sns_topic.request_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.request_updates_3.arn
-}
-
-resource "aws_sns_topic_subscription" "request_updates_sqs_target4" {
-  topic_arn = aws_sns_topic.request_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.request_updates_4.arn
-}
-
-resource "aws_sns_topic_subscription" "request_updates_sqs_target5" {
-  topic_arn = aws_sns_topic.request_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.request_updates_5.arn
 }
 
 # # We create this in deploy-tf.sh for now as the order matters, so we make the repo,
